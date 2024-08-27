fdad9ebfc90318104e6ddf5d3a0ddddb9cba8577
jahnavreddy <jahnavredy12@gmail.com>
2024-08-27 11:47:29 +0530

    classes and objects

---
+++ MainClass.java
@@ -0,0 +1,8 @@
+public class MainClass {
+
+  public static void main(String[] args) {
+    Patients Milan = new Patients("Milan", "sana" , "milan73@" ,6969);
+    System.out.println(Milan.getEmail());
+
+  }
+}
