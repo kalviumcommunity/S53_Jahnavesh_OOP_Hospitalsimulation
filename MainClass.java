336eb8edf27e0cc3d8323cc1fc039fbfe70a5b60
jahnavreddy <jahnavredy12@gmail.com>
2024-08-28 09:11:31 +0530

    removed files

--- MainClass.java
+++
@@ -1,8 +0,0 @@
-public class MainClass {
-
-  public static void main(String[] args) {
-    Patients Milan = new Patients("Milan", "sana" , "milan73@" ,6969);
-    System.out.println(Milan.getEmail());
-
-  }
-}
