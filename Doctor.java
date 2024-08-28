2b474b6d70af5c8330c548bfe52ac756b4600f0b
jahnavreddy <jahnavredy12@gmail.com>
2024-08-28 10:35:17 +0530

    made some changes

--- Doctor.java
+++ Doctor.java
@@ -1,14 +1,34 @@
 public class Doctor {
   private String specialization;
+  private String duty;
+
+  public Doctor(String specialization, String duty) {
+      this.specialization = specialization;
+      this.duty = duty;
+  }
+
+  public String getDuty() {
+      return duty;
+  }
+
+  public void setDuty(String duty) {
+      this.duty = duty;
+  }
 
   public String getSpecialization() {
-    return specialization;
+      return specialization;
   }
 
   public void setSpecialization(String specialization) {
-    this.specialization = specialization;
+      this.specialization = specialization;
   }
 
-  
+  public String displayInfo() {
+      return "Specialization: " + specialization + "\nDuty: " + duty;
+  }
 
+  public void assignDuty(String duty) {
+      this.duty = duty;
+      System.out.println("Doctor with specialization in " + specialization + " assigned to " + duty + ".");
+  }
 }
--- Mainclass.java
+++ Mainclass.java
@@ -2,8 +2,15 @@ class MainClass {
 
   public static void main(String[] args) {
     Patients Ram = new Patients("Ram", "Kumar" , "Ram123@" ,12345);
+    Patients Raju = new Patients("Raju", "Ridhi" , "RajuRidhi@gmail.com" ,94402);
+    Doctor doctor1 = new Doctor("Cardiology", "Night Shift");
+    Doctor doctor2 = new Doctor("Neurology", "Day Shift");
+    System.out.println(doctor1.displayInfo());
     System.out.println(Ram.getEmail());
     System.out.println(Ram.getFullName());
+    System.out.println(Ram.validateEmail());
+    System.out.println(Raju.validateEmail());
+    System.out.println();
 
   }
 }
