0ec13579a0844c31582b4cc6e679ed831af3a74c
jahnavreddy <jahnavredy12@gmail.com>
2024-08-29 11:01:33 +0530

    Changed main

--- Mainclass.java
+++ Mainclass.java
@@ -1,28 +1,15 @@
-import java.util.ArrayList;
-
 class MainClass {
 
   public static void main(String[] args) {
-    ArrayList<Patients> patientsList = new ArrayList<>();
-    patientsList.add(new Patients("Ram", "Kumar", "Ram123@", 12345));
-    patientsList.add(new Patients("Raju", "Ridhi", "RajuRidhi@gmail.com", 94402));
+    Patients Ram = new Patients("Ram", "Kumar" , "Ram123@" ,12345);
+    Patients Raju = new Patients("Raju", "Ridhi" , "RajuRidhi@gmail.com" ,94402);
+    Doctor doctor1 = new Doctor("Cardiology", "Night Shift");
+    Doctor doctor2 = new Doctor("Neurology", "Day Shift");
+    System.out.println(doctor1.displayInfo());
+    System.out.println(Ram.getEmail());
+    System.out.println(Ram.getFullName());
+    System.out.println(Ram.validateEmail());
+    System.out.println(Raju.validateEmail());
 
-    for (int i = 0; i < patientsList.size(); i++) {
-      patientsList.get(i).assignID(i + 1);
-    }
-
-    ArrayList<Doctor> doctorsList = new ArrayList<>();
-    doctorsList.add(new Doctor("Cardiology", "Night Shift"));
-    doctorsList.add(new Doctor("Neurology", "Day Shift"));
-
-    for (Doctor doctor : doctorsList) {
-      System.out.println(doctor.displayInfo());
-    }
-
-    for (Patients patient : patientsList) {
-      System.out.println(patient.displayInfo());
-      System.out.println("Is email valid? " + patient.validateEmail());
-      System.out.println();
-    }
   }
 }
