53e13b384972e69fc99a136e957ec01207ee2425
jahnavreddy <jahnavredy12@gmail.com>
2024-08-29 10:51:26 +0530

    created array of objects

--- Doctor.java
+++ Doctor.java
@@ -1,34 +1,35 @@
 public class Doctor {
-  private String specialization;
-  private String duty;
-
-  public Doctor(String specialization, String duty) {
+    private String specialization;
+    private String duty;
+  
+    public Doctor(String specialization, String duty) {
       this.specialization = specialization;
       this.duty = duty;
-  }
-
-  public String getDuty() {
+    }
+  
+    public String getDuty() {
       return duty;
-  }
-
-  public void setDuty(String duty) {
+    }
+  
+    public void setDuty(String duty) {
       this.duty = duty;
-  }
-
-  public String getSpecialization() {
+    }
+  
+    public String getSpecialization() {
       return specialization;
-  }
-
-  public void setSpecialization(String specialization) {
+    }
+  
+    public void setSpecialization(String specialization) {
       this.specialization = specialization;
-  }
-
-  public String displayInfo() {
+    }
+  
+    public String displayInfo() {
       return "Specialization: " + specialization + "\nDuty: " + duty;
-  }
-
-  public void assignDuty(String duty) {
+    }
+  
+    public void assignDuty(String duty) {
       this.duty = duty;
       System.out.println("Doctor with specialization in " + specialization + " assigned to " + duty + ".");
+    }
   }
-}
+  
